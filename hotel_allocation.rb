557611b2e05edb99e54fe1f9ea8f10e409658187
Toby Muir-Little <toby.muir-little@simplybusiness.co.uk>
2025-07-31 17:21:25 +0100

    use slightly better performance method

--- hotel_allocation.rb
+++ hotel_allocation.rb
@@ -5,18 +5,12 @@ def allocate_rooms(customers)
     room_storage[hotel_room] = [] if room_storage[hotel_room].nil?
 
     customer_nights_spent_in_room = (customer[0]..customer[1]).to_a
-    p "room #{hotel_room} storage: #{room_storage[hotel_room]}"
-    p "nights in room #{customer_nights_spent_in_room}"
-
-    until room_storage[hotel_room].intersection(customer_nights_spent_in_room).empty?
+    until (room_storage[hotel_room] & customer_nights_spent_in_room).empty?
       hotel_room += 1
-      p "room #{hotel_room} storage: #{room_storage[hotel_room]}"
-      p "nights in room #{customer_nights_spent_in_room}"
       room_storage[hotel_room] = [] if room_storage[hotel_room].nil?
     end
 
     room_storage[hotel_room] += customer_nights_spent_in_room
-    p "room storage after changes #{room_storage}"
     arr << hotel_room
   end
 end
