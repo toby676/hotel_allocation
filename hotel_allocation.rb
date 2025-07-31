b9e270d1d4bb64720d86d23777a9d910013889f0
Toby Muir-Little <toby.muir-little@simplybusiness.co.uk>
2025-07-31 16:17:18 +0100

    use each_with_object

--- hotel_allocation.rb
+++ hotel_allocation.rb
@@ -1,9 +1,5 @@
 def allocate_rooms(customers)
-  arr = []
-
-  (0..customers.length - 1).each do |customer|
+  (0..customers.length - 1).each_with_object([]) do |customer, arr|
     arr << customer + 1
   end
-
-  arr
 end
