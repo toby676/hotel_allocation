21d151fcdb8e39004cf7e1ac84b2bfc242db44be
Toby Muir-Little <toby.muir-little@simplybusiness.co.uk>
2025-08-01 09:58:59 +0100

    Copilot to save the day

--- hotel_allocation.rb
+++ hotel_allocation.rb
@@ -1,20 +1,29 @@
 def allocate_rooms(customers)
-  room_storage = Hash.new { |hash, key| hash[key] = [] }
+  room_last_departure = []
 
   sorted_customers = customers.each_with_index.sort_by { |(customer, index)| [customer[1], customer[0]] }
   result = Array.new(customers.length)
 
   sorted_customers.each do |customer, original_index|
-    hotel_room = 1
     check_in, check_out = customer
-    nights = (check_in..check_out).to_a
 
-    until !room_storage[hotel_room].any? { |night| nights.include?(night) }
-      hotel_room += 1
+    best_room = nil
+    latest_departure = 0
+
+    room_last_departure.each_with_index do |last_departure, room_index|
+      if last_departure < check_in && last_departure > latest_departure
+        best_room = room_index
+        latest_departure = last_departure
+      end
     end
 
-    room_storage[hotel_room] += nights
-    result[original_index] = hotel_room
+    if best_room
+      room_last_departure[best_room] = check_out
+      result[original_index] = best_room + 1
+    else
+      room_last_departure << check_out
+      result[original_index] = room_last_departure.length
+    end
   end
 
   result
