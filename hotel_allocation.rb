6c46afda29bf0cc3272e0af1f9d81c2a280a01c0
Toby Muir-Little <toby.muir-little@simplybusiness.co.uk>
2025-08-01 09:39:47 +0100

    better naming for variables

--- hotel_allocation.rb
+++ hotel_allocation.rb
@@ -1,10 +1,12 @@
 def allocate_rooms(customers)
   room_storage = {}
+
   customers.each_with_object([]) do |customer, arr|
     hotel_room = 1
+    check_in, check_out = customer
     room_storage[hotel_room] = [] if room_storage[hotel_room].nil?
 
-    customer_nights_spent_in_room = (customer[0]..customer[1]).to_a
+    customer_nights_spent_in_room = (check_in..check_out).to_a
     until (room_storage[hotel_room] & customer_nights_spent_in_room).empty?
       hotel_room += 1
       room_storage[hotel_room] = [] if room_storage[hotel_room].nil?
