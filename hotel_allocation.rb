b681dc545eea90655f4e5c97cbaf2d79987b398b
Toby Muir-Little <toby.muir-little@simplybusiness.co.uk>
2025-08-01 09:48:42 +0100

    More efficient?

--- hotel_allocation.rb
+++ hotel_allocation.rb
@@ -6,7 +6,7 @@ def allocate_rooms(customers)
     check_in, check_out = customer
 
     customer_nights_spent_in_room = (check_in..check_out).to_a
-    until (room_storage[hotel_room] & customer_nights_spent_in_room).empty?
+    until !room_storage[hotel_room].any? { |night| customer_nights_spent_in_room.include?(night) }
       hotel_room += 1
     end
 
